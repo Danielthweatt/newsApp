e9a34c6aef45155ed30befc5f1526d5eaa77e15d
Danielthweatt <dthweatt192@yahoo.com>
2018-06-20 14:17:15 -0500

    added test folder, scaffolded and style index.handlebars, worked on scraping

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,39 @@
 // Dependencies
 const express = require("express");
+const request = require("request");
+const cheerio = require("cheerio");
 
 // Router Setup
 const router = express.Router();
 
 // Routes
 router.get("/", function(req, res){
-    const hbsObject = {
-        articles: ["Article One", "Article Two", "Article Three"]
-    };
-    res.render("index", hbsObject);
+    request("https://www.deadspin.com/", function(error, response, html){
+        const $ = cheerio.load(html);
+        const results = [];
+        let link;
+        let title;
+        let headlineElement;
+        let summary;
+        $("div.post-wrapper").each(function(i, element){
+            headlineElement = $("h1.headline", element);
+            link = headlineElement.children().attr("href");
+            title = headlineElement.children().text();
+            summary = $("div.entry-summary", element).children().text();
+            if (title) {
+                results.push({
+                    title: title,
+                    link: link,
+                    summary: summary
+                });
+            }
+        });
+        const hbsObject = {
+            articles: results
+        };
+        res.render("index", hbsObject);
+    });
 });
-
-
 
 // Export
 module.exports = router;
